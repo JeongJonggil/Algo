432c007b322cbe5cb413f6879996c8f78b489492
JeongJonggil <whdrlf3928@naver.com>
2023-12-16 11:06:50 +0900

    231216 열심히 합시다

--- 정종길/이코테 문제/구현/구현-11-뱀.py
+++ 정종길/이코테 문제/구현/구현-11-뱀.py
@@ -31,7 +31,7 @@ Arr[ni][nj] = 2
 while True:
     time += 1
 
-    if check_idx < l and time == int(input_dir[check_idx][0]):
+    if check_idx < l and time - 1 == int(input_dir[check_idx][0]): # ex) input_dir이 [3, "D"]면, 3초까지는 가던방향으로 진행하고 4초 때 바뀐 방향으로 진행하므로 time-1을 비교해야됨
         if input_dir[check_idx][1] == "D":
             dir_idx = (dir_idx + 1) % 4
         else:
@@ -42,8 +42,7 @@ while True:
     nj += dir[dir_idx][1]
     q.append([ni,nj])       # 일단 몸통에 추가
 
-    if ni >= n or ni < 0 or nj >= n or nj <0 or Arr[ni][nj] == 2: # 뱀 머리가 맵을 벗어나거나 몸에 부딪힐 경우
-        print(time, q)
+    if ni >= n or ni < 0 or nj >= n or nj < 0 or Arr[ni][nj] == 2: # 뱀 머리가 맵을 벗어나거나 몸에 부딪힐 경우
         break
 
     if Arr[ni][nj] == 1:    # 사과를 밟을 경우
@@ -54,5 +53,6 @@ while True:
         Arr[ni][nj] = 2     # 몸으로 바꾸기
         bi, bj = q.popleft() # 리스트에 맨 먼저 들어갔던게 뱀 꼬리이므로 popleft로 뱀 꼬리 인덱스 추출
         Arr[bi][bj] = 0      # 뱀 꼬리 부분 땅으로 초기화
+
 
 print(time)
